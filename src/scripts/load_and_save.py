98b3124fc1592bb6378e14e8cef8775150e3c059
MAUD DUPONT-ROC <maud.dupont-roc@epfl.ch>
2024-11-15 21:59:50 +0000

    Cleaned notebook

--- src/scripts/load_and_save.py
+++ src/scripts/load_and_save.py
@@ -2,7 +2,7 @@ import zipfile
 import pandas as pd
 
 # Load the whole dataset here as a data frame
-def load_data(zip_file_path = '/Users/barbaragrosjean/Desktop/ada-2024-project-laambada/data/BindingDB_All.tsv.zip', tsv_file_name = 'BindingDB_All.tsv'):
+def load_data(zip_file_path = 'data/BindingDB_All_tsv.zip', tsv_file_name = 'BindingDB_All.tsv'):
     # Open the zip file
     with zipfile.ZipFile(zip_file_path, 'r') as z:
         # Open the TSV file within the zip
