d8f0817c232e3cad90ef0dd7c013172feefe1f9e
sallinen <alex.sallinen@gmail.com>
2024-11-25 09:23:14 +0100

    T-SNE + PCA

--- src/scripts/load_and_save.py
+++ src/scripts/load_and_save.py
@@ -11,3 +11,10 @@ def load_data(zip_file_path = '/Users/barbaragrosjean/Desktop/ada-2024-project-l
             df = pd.read_csv(file, sep='\t', on_bad_lines='skip')
 
     return df
+
+def load_data_direct(tsv_file_name = 'data/BindingDB_All.tsv'):
+    with open(tsv_file_name) as file:
+            # Read the TSV file into a DataFrame
+            df = pd.read_csv(file, sep='\t', on_bad_lines='skip', usecols=["Ligand SMILES"])
+
+    return df
