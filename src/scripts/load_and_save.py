94debe800b68d5975f64db5f863ee1c5b9c2f555
maud73 <maud.dupont-roc@epfl.ch>
2024-12-02 12:59:30 +0100

    Organised

--- src/scripts/load_and_save.py
+++ src/scripts/load_and_save.py
@@ -2,7 +2,7 @@ import zipfile
 import pandas as pd
 
 # Load the whole dataset here as a data frame
-def load_data(zip_file_path = 'data/BindingDB_All_tsv.zip', tsv_file_name = 'BindingDB_All.tsv'):
+def load_full_dataset(zip_file_path = 'data/BindingDB_All_tsv.zip', tsv_file_name = 'BindingDB_All.tsv'):
     # Open the zip file
     with zipfile.ZipFile(zip_file_path, 'r') as z:
         # Open the TSV file within the zip
@@ -10,4 +10,8 @@ def load_data(zip_file_path = 'data/BindingDB_All_tsv.zip', tsv_file_name = 'Bin
             # Read the TSV file into a DataFrame
             df = pd.read_csv(file, sep='\t', on_bad_lines='skip')
 
+    return df
+
+def load_data(zip_file_path = 'src/data/clean_subset.csv.zip'):
+    df = pd.read_csv(zip_file_path, compression='zip')
     return df
