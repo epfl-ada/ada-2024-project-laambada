f3d0a5bedb71bba8c354e1629421bfbfbeceef5b
unknown <laura.gambaretto@epfl.ch>
2024-12-12 10:09:46 +0100

    Take last version src folder

--- src/scripts/load_and_save.py
+++ src/scripts/load_and_save.py
@@ -2,7 +2,7 @@ import zipfile
 import pandas as pd
 
 # Load the whole dataset here as a data frame
-def load_data(zip_file_path = '/Users/barbaragrosjean/Desktop/ada-2024-project-laambada/data/BindingDB_All.tsv.zip', tsv_file_name = 'BindingDB_All.tsv'):
+def load_full_dataset(zip_file_path = 'data/BindingDB_All_tsv.zip', tsv_file_name = 'BindingDB_All.tsv'):
     # Open the zip file
     with zipfile.ZipFile(zip_file_path, 'r') as z:
         # Open the TSV file within the zip
@@ -12,9 +12,6 @@ def load_data(zip_file_path = '/Users/barbaragrosjean/Desktop/ada-2024-project-l
 
     return df
 
-def load_data_direct(tsv_file_name = 'data/BindingDB_All.tsv'):
-    with open(tsv_file_name) as file:
-            # Read the TSV file into a DataFrame
-            df = pd.read_csv(file, sep='\t', on_bad_lines='skip', usecols=["Ligand SMILES"])
-
+def load_data(zip_file_path = 'src/data/clean_subset.csv.zip'):
+    df = pd.read_csv(zip_file_path, compression='zip')
     return df
